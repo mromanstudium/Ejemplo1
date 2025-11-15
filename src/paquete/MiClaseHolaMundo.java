e34a2c9e224856affde7542ab2ad2dda15fa7af4
unknown <mromanstudium>
2025-11-15 19:04:20 +0100

    comentario desde rama

--- src/paquete/MiClaseHolaMundo.java
+++ src/paquete/MiClaseHolaMundo.java
@@ -4,7 +4,7 @@ public class MiClaseHolaMundo {
 
 	public static void main(String[] args) {
 		System.out.print("Hola clase de ED! que bieen!!");
-
+		System.out.print("desde la rama nueva");
 	}
 
 }
