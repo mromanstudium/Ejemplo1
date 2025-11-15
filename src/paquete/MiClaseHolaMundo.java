1b3602c80be4627e26dcd04699a56b24c42aeae8
unknown <mromanstudium>
2025-11-15 19:15:07 +0100

    arreglo del botón. Closes #1

--- src/paquete/MiClaseHolaMundo.java
+++ src/paquete/MiClaseHolaMundo.java
@@ -5,6 +5,7 @@ public class MiClaseHolaMundo {
 	public static void main(String[] args) {
 		System.out.print("Hola clase de ED! que bieen!!");
 		System.out.print("desde la rama nueva");
+		System.out.print("subida issue");
 	}
 
 }
