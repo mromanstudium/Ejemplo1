572ac81329e382515cdf5656e76214403580a93f
unknown <mromanstudium>
2025-11-24 16:33:57 +0100

    prueba issue

--- src/paquete/MiClaseHolaMundo.java
+++ src/paquete/MiClaseHolaMundo.java
@@ -6,6 +6,7 @@ public class MiClaseHolaMundo {
 		System.out.print("Hola clase de ED! que bieen!!");
 		System.out.print("desde la rama nueva");
 		System.out.print("subida issue");
+		System.out.print("subida issue2");
 	}
 
 }
